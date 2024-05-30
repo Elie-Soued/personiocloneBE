f5f58428e076292df2c9eb549b9bc99667747522
elie <elie.soued@tech11.com>
2024-05-30 12:45:13 +0200

    remove useless comments

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,15 @@ import usersRoute from "./Routes/users";
 import dotenv from "dotenv";
 dotenv.config();
 
-//Constants declarations
 const { PORT } = process.env;
 const app: Express = express();
 
-//Use Body Parser to format the body´s reponse
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-//Assigning a route file to a path
 app.use("/users", usersRoute);
 
 app.get("/", (req: Request, res: Response) => res.send("Hello World"));
 
-//Starting a server and make it listen to a specific port
 app.listen(PORT, () => console.log(`Server running on port${PORT}`));
